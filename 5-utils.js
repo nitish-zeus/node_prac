b918adc174e9e4988fc884869b15716702c24f38
Nitish Kumar <nitishkumar@Netopss-MacBook-Pro.local>
2021-07-16 22:26:11 +0530

    initial commit

---
+++ 5-utils.js
@@ -0,0 +1,4 @@
+const sayHi = (name) => {
+    console.log(` hi there ${name}`)
+}
+module.exports = sayHi
---
+++ 6-alternative-flav.js
@@ -0,0 +1,6 @@
+module.exports.items = ['item1','item2']
+const person = {
+name : "Stefan"
+}
+
+module.exports.singlePerson = person
